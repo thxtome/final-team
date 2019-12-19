c3ec853798126f21366da00dafc97efa85d35ee9
SM <seoungmin104@naver.com>
2019-12-19 18:59:28 +0900

    인터셉터 구현

---
+++ doublecome/src/main/java/kr/co/doublecome/common/controller/Interceptor.java
@@ -0,0 +1,26 @@
+package kr.co.doublecome.common.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+ 
+public class Interceptor extends HandlerInterceptorAdapter{
+ 
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        if (request.getUserPrincipal() == null ){
+            response.sendRedirect("/doublecome/user/loginForm.do");
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+}
+
