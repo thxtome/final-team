f5a3e86551412c7084c5c8d5651b559ea2816dbc
namsu91 <namsu9191@gmail.com>
2019-12-22 15:47:39 +0900

    마이페이지 입찰 리스트 경매 남은시간 표시

--- doublecome/src/main/webapp/resources/js/user/userInfo.js
+++ doublecome/src/main/webapp/resources/js/user/userInfo.js
@@ -11,32 +11,26 @@
 		
 		
 		let Dday = new Date(`${format(result[0].auctionLimitDate, "hms")}`);
-	/*	setInterval( () => {*/
-		let day = new Date();
-		
-		let remainTime = Dday - day
-		let rday = new Date(remainTime);
-		console.log(rday ,'rday');
-		console.log(`${format(rday, "Mdhms")}` ,'rday');
-		$("#remainTime").html(`${format(rday, "Mdhms")}`)
-		/*}, 1000);*/
-		
-		
-		CountDownTimer(rday, 'remainTime'); // 2017년 1월 1일까지
 
-		function CountDownTimer(dt, id)
-		{
-		var end = new Date(dt);
+		
+		console.log(`${format(result[0].auctionLimitDate, "hms")}` ,'time');
+		console.log(Dday ,'Dday');
+		
+		
+		
 
-		var _second = 1000;
-		var _minute = _second * 60;
-		var _hour = _minute * 60;
-		var _day = _hour * 24;
-		var timer;
+		function CountDownTimer(dt, id){
+		let end = new Date(dt);
+
+		let _second = 1000;
+		let _minute = _second * 60;
+		let _hour = _minute * 60;
+		let _day = _hour * 24;
+		let timer;
 
 		function showRemaining() {
-		var now = new Date();
-		var distance = end - now;
+		let now = new Date();
+		let distance = end - now;
 		if (distance < 0) {
 
 		clearInterval(timer);
@@ -44,15 +38,26 @@
 
 		return;
 		}
-		var days = Math.floor(distance / _day);
-		var hours = Math.floor((distance % _day) / _hour);
-		var minutes = Math.floor((distance % _hour) / _minute);
-		var seconds = Math.floor((distance % _minute) / _second);
-
-		document.getElementById(id).innerHTML = days + '일 ';
-		document.getElementById(id).innerHTML += hours + '시간 ';
-		document.getElementById(id).innerHTML += minutes + '분 ';
-		document.getElementById(id).innerHTML += seconds + '초';
+		let days = Math.floor(distance / _day);
+		let hours = Math.floor((distance % _day) / _hour);
+		let minutes = Math.floor((distance % _hour) / _minute);
+		let seconds = Math.floor((distance % _minute) / _second);
+		
+		if(days != 0 && hours != 0){
+			
+			document.getElementById(id).innerHTML = days + '일 ';
+			document.getElementById(id).innerHTML += hours + '시간 ';
+			document.getElementById(id).innerHTML += minutes + '분 ';
+			/*document.getElementById(id).innerHTML += seconds + '초';*/
+		}else if(days == 0 && hours != 0){
+			document.getElementById(id).innerHTML = hours + '시간 ';
+			document.getElementById(id).innerHTML += minutes + '분 ';
+			/*document.getElementById(id).innerHTML += seconds + '초';*/
+		}else if(days == 0 && hours == 0)
+			document.getElementById(id).innerHTML = minutes + '분 ';
+			/*document.getElementById(id).innerHTML += seconds + '초';*/
+		
+		
 		}
 
 		timer = setInterval(showRemaining, 1000);
@@ -61,17 +66,19 @@
 		
 		for(i = 0; i < result.length; i++){
 			console.log(result[i]);
+			console.log(Dday, `${result[i].auctionCnt}`);
 			$('#bidList').append(
 				`<div class="card_container${result[i].auctionCnt % 2}">
 				<a id="aution" href="/doublecome/auction/detailAuction.do?no=${result[i].auctionNo}&userEmail=${result[i].userEmail}">
 					<img src="/doublecome/file/downLoadFile.do?fileNo=${result[i].fileNo}" alt="" class="card_image" >
 					<div class="aution_title"><div class="card_autionTitle">${result[i].auctionTitle}</div></div>
 					<div class="card_sub_now">즉시 구매 가:<span class="card_content">${result[i].auctionBuyNow}</span></div>
-					<div class="card_sub_date">경매 마감 일:<span id="remainTime" class="card_content">${format(result[i].auctionLimitDate, "Mdhms")}</span></div>
+					<div class="card_sub_date">남은 시간:<span id="remainTime${result[i].auctionCnt}" class="card_content">${format(result[i].auctionLimitDate, "Mdhms")}</span></div>
 				</a>				
 				</div>`
 			);
 
+			CountDownTimer(`${format(result[i].auctionLimitDate, "hms")}`, `remainTime${result[i].auctionCnt}`); 
 		}	
 		
 		
@@ -87,25 +94,7 @@
 
 
 
-$(document).on("click", ".asd", () => {
-	Swal.fire({
-		title: '탈퇴 하시겠습니까',
-		/*text: "회원가입 혹은 로그인후 진행해주세요",*/
-		type: 'warning',
-		showCancelButton: true,
-		confirmButtonColor: '#3085d6',
-		cancelButtonColor: '#d33',
-		confirmButtonText: '확인',
-		cancelButtonText: '취소'
-	}).then((result) => {
-		if (result.value) {
-			return true;
-		} else if (result.dismiss === Swal.DismissReason.cancel) {
-			return false;
-		}
-	})
-	
-})
+
 
 $(document).on("click", "#deleteBtn", () => {
 	Swal.fire({
