06be86d502b77429e9304172383137953373bf57
namsu park <namsu9191@gmail.com>
2020-01-03 11:58:40 +0900

    비밀번호 찾기 입력된 메일로

--- doublecome/src/main/java/kr/co/doublecome/util/mail/MailSendController.java
+++ doublecome/src/main/java/kr/co/doublecome/util/mail/MailSendController.java
@@ -33,23 +33,13 @@ public class MailSendController {
 			public void prepare(MimeMessage mimeMessage) throws Exception { 
 				final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8"); 
 				helper.setFrom("doublecome4@gmail.com");  					// 보내는사람 생략하면 정상작동을 안함 xml과 다르면 전송 안됨
-				helper.setTo("doublegoo@naver.com"/*email*/); 						// 받는사람 이메일
+				helper.setTo(email); 						// 받는사람 이메일
 				helper.setSubject("누구나 쉬운 경매! 더불로와 - 비밀번호 찾기"); 			// 메일제목은 생략이 가능하다
 				
 				
 				
 				helper.setText(
-						/*
-						 * "<div>"+ "<div style='color: #8e8e8e'> 누구나 쉬운 경매!</div>" +
-						 * "<div style='color: #DB1D3B;'> 더불 </div>" +
-						 * "<div style='color: #DB1D3B;'> 러와 </div>" + "<div>" +
-						 * "<div class=\"pen-title\" style=''>\r\n" +
-						 * "	  <div><a href=\"doublecome/main.do\">누구나 쉬운 경매!</a></div>\r\n" +
-						 * "		<div><a class=\"titleToMain\" href=\"doublecome/main.do\">더블</a></div>\r\n"
-						 * +
-						 * "		<div><a class=\"titleToMain\" href=\"doublecome/main.do\">로와</a></div>\r\n"
-						 * + "	</div>" +
-						 */
+						
 						"본인이 요청하지 않았다면 무시해 주세요<br>" +
 						"<br>비밀번호 수정 페이지로 " +
 						"<a href=http://localhost:80/doublecome/user/findPassForm.do"+
